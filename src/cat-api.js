24071af7af1a8a820619d1a22e4ba30a926613b8
DevMasterX <sokol080288@gmail.com>
2023-08-10 22:25:06 +0300

    cat-api.js - 1

---
+++ src/cat-api.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+const API_KEY =
+  'live_8c6pn5exD7MB9obFr3gpVQfAVmf4bK5uZbksK5ZOFZCfy9EF2x3IN7ynkUMlVw5p';
+axios.defaults.headers.common['x-api-key'] = API_KEY;
+
+const BASE_URL = 'https://api.thecatapi.com/v1';
---
+++ src/index.js
@@ -0,0 +1,2 @@
+// live_8c6pn5exD7MB9obFr3gpVQfAVmf4bK5uZbksK5ZOFZCfy9EF2x3IN7ynkUMlVw5p;
+import axios from 'axios';
